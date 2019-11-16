218c2d8c7176bf50887dd70c4050556c48f4bf50
Aleksei Semin <alllexsm@gmail.com>
2019-11-16 17:33:09 +0200

    Add bootstrap-vue

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>Fincredible filters</h1>
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col class="filter-panel" cols="4">
+          <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+          </ol>
+        </b-col>
+        <b-col class="content-panel">
+          <div>
+            <b-card
+              title="Card Title"
+              img-src="https://picsum.photos/600/300/?image=25"
+              img-alt="Image"
+              img-top
+              tag="article"
+              style="max-width: 20rem;"
+              class="mb-2"
+            >
+              <b-card-text>Some quick example text to build on the card title and make up the bulk of the card's content.</b-card-text>
+              <b-button href="#" variant="primary">Go somewhere</b-button>
+            </b-card>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "./components/HelloWorld.vue";
 
 @Component({
   components: {
-    HelloWorld,
-  },
+    HelloWorld
+  }
 })
 export default class App extends Vue {}
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+</style>
+
+<style scoped>
+.filter-panel {
+  border: 1px solid black;
+}
+.content-panel {
+  border: 1px solid black;
+}
+.card {
+  margin: 10px;
 }
 </style>
